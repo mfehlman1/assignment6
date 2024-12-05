b4af7d6bac88286b6132ed59846ce5d7990bea09
mfehlman1 <161093831+mfehlman1@users.noreply.github.com>
2024-12-04 19:53:32 -0800

    Update models.py

--- apps/contact_us/models.py
+++ apps/contact_us/models.py
@@ -14,8 +14,6 @@ db.define_table(
     Field('email', 'string', requires=IS_EMAIL(), label='Email'),
     Field('phone', 'string', requires=IS_NOT_EMPTY(), label='Phone'),
     Field('message', 'text', label='Message'),
-    Field('created_at', 'datetime', default=get_time, writable=False, readable=False),
 )
-
 db.commit()
 
