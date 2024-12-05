b98e6155bdb9877b7eec4a87235ef7308e423e07
mfehlman1 <161093831+mfehlman1@users.noreply.github.com>
2024-12-04 23:46:28 -0800

    update

--- apps/contact_us/controllers.py
+++ apps/contact_us/controllers.py
@@ -11,8 +11,12 @@ from py4web.utils.grid import Grid, GridClassStyleBulma
 @action.uses('index.html', db, session, T)
 def index():
     form = Form(db.contact_requests, csrf_session=session, formstyle=FormStyleBulma)
-    print("Form object type:", type(form))
-    print("Form XML output:", form.xml())
+    print("Form object", form)
+    if form.accepted:
+        print("Form accepted with:", form.vars)
+        redirect(URL('index'))
+    elif form.errors:
+        print("Errors: ", form.errors)
     return dict(form=form)
 
 @action('contact_requests', method=['GET', 'POST'])
